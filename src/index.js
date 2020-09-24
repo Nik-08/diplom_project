883c16806d3a2b11c09678a5ed2b8479c8409083
Nik-08 <69086824+Nik-08@users.noreply.github.com>
2020-09-24 23:32:03 +0700

    feat(24.09.2020) textModal

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import elementClosest from 'element-closest';
 elementClosest(window);
 
 import dropMenu from './modules/dropMenu';
-
+import textModal from './modules/textModal';
 
 
 
 
 //dropMenu
 dropMenu();
+//textModal
+textModal();
---
+++ src/modules/textModal.js
@@ -0,0 +1,24 @@
+const textModal = () => {
+  const  headerMain = document.querySelector('.header-main'),
+  popup = document.getElementById('free_visit_form'),
+  formContent = popup.querySelector('.form-content'),
+  closeIcom = popup.querySelector('.close_icon');
+  headerMain.addEventListener('click', (event) => {
+    let target = event.target;
+     if(target.closest('.open-popup')){
+      popup.style.display = 'block';
+     }
+  });
+  popup.addEventListener('click', (event) =>{
+    let target = event.target;
+    if(target.classList.contains('close_icon')){
+      popup.style.display = 'none';
+    }else{
+      target = target.closest('.form-content');
+    }
+      if(!target){
+        popup.style.display = 'none';
+      }
+  });
+};
+export default textModal;
